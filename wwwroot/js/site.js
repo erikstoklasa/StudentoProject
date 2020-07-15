dcae0c76503929b60750796f52e705a5d95fbeb3
Erik Stoklasa <45731908+erikstoklasa@users.noreply.github.com>
2020-07-15 14:54:34 +0200

    Added student search for admins

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,18 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
 
-// Write your Javascript code.
+var searchUsers = document.querySelector('#search'),
+    users = document.querySelectorAll('tbody tr'),
+    usersData = document.querySelectorAll('.search-data'),
+    searchVal;
+
+searchUsers.addEventListener('keyup', function () {
+    searchVal = this.value.trim().toLowerCase();
+    for (var i = 0; i < users.length; i++) {
+        if (!searchVal || usersData[i].textContent.toLowerCase().indexOf(searchVal) > -1) {
+            users[i].style['display'] = 'table-row';
+        }
+        else {
+            users[i].style['display'] = 'none';
+        }
+    }
+});

276d9c83119cf9b3fe4e2b10ef9d4df91fa230f9
Erik Stoklasa <45731908+erikstoklasa@users.noreply.github.com>
2020-07-17 12:21:28 +0200

    Added: - student and teacher services - teacher validation service and validation utils

---
+++ Services/TeacherService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolGradebook.Data;
+using SchoolGradebook.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolGradebook.Services
+{
+    public class TeacherService
+    {
+        private readonly SchoolContext context;
+
+        public TeacherService(SchoolContext context)
+        {
+            this.context = context;
+        }
+        public async Task<int> GetTeacherId(string userAuthId)
+        {
+            Teacher teacher = await context.Teachers
+                .Where(t => t.UserAuthId == userAuthId)
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+            return teacher.Id;
+        }
+        public async Task<Teacher> GetTeacherAsync(int teacherId)
+        {
+            Teacher teacher = await context.Teachers
+                .FindAsync(teacherId);
+            return teacher;
+        }
+        public async Task<Teacher[]> GetAllTeachersAsync()
+        {
+            Teacher[] teacher = await context.Teachers
+                .AsNoTracking()
+                .OrderBy(s => s.LastName)
+                .ToArrayAsync();
+            return teacher;
+        }
+        public async Task<int> GetTeacherCountAsync()
+        {
+            return (await GetAllTeachersAsync()).Length;
+        }
+    }
+}
---
+++ Services/ValidationUtils.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolGradebook.Services
+{
+    public static class ValidationUtils
+    {
+        public static bool PersonalIdentifNumberIsValid(string number)
+        {
+            if (string.IsNullOrWhiteSpace(number))
+            {
+                return false;
+            }
+            number = number.Trim();
+
+            number = RemoveSlashIfExists(number);
+
+            Int64 convertedNumber;
+            try
+            {
+                convertedNumber = Int64.Parse(number);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            if (convertedNumber % 11 != 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private static string RemoveSlashIfExists(string number)
+        {
+            int indexOfSlash = number.IndexOf('/');
+            if (indexOfSlash >= 0)
+            {
+                number = number.Remove(indexOfSlash, 1);
+            }
+
+            return number;
+        }
+    }
+}

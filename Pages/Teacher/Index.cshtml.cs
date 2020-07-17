2fe30a617c80a382ba740de2535524c4faf5a2ff
Erik Stoklasa <45731908+erikstoklasa@users.noreply.github.com>
2020-07-17 16:27:04 +0200

    Improved language and design consistency

--- Pages/Teacher/Index.cshtml.cs
+++ Pages/Teacher/Index.cshtml.cs
@@ -13,19 +13,28 @@ namespace SchoolGradebook.Pages.Teacher
     public class IndexModel : PageModel
     {
         private readonly Analytics _analytics;
+        private readonly TeacherService teacherService;
+        private readonly StudentService studentService;
+
         public int StudentCount { get; set; }
+        public int UniqueStudentCount { get; set; }
         public int SubjectCount { get; set; }
         private string UserId { get; set; }
 
-        public IndexModel(Analytics analytics, IHttpContextAccessor httpContextAccessor)
+        public IndexModel(Analytics analytics, IHttpContextAccessor httpContextAccessor, TeacherService teacherService, StudentService studentService)
         {
             _analytics = analytics;
+            this.teacherService = teacherService;
+            this.studentService = studentService;
             UserId = httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
         }
         public async Task OnGetAsync()
         {
             StudentCount = await _analytics.GetStudentsCountByTeacherUserAuthIdAsync(UserId);
             SubjectCount = await _analytics.GetSubjectsCountByTeacherIdAsync(UserId);
+
+            int teacherId = await teacherService.GetTeacherId(UserId);
+            UniqueStudentCount = (await studentService.GetAllStudentsByTeacherAsync(teacherId)).Count;
         }
     }
 }

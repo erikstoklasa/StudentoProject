733bd77d809d3c5f0f05883426a0f7ac367b2afa
Erik Stoklasa <45731908+erikstoklasa@users.noreply.github.com>
2020-09-04 11:41:27 +0200

    Updated landing page and footer design

---
+++ Services/SchoolService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolGradebook.Data;
+using SchoolGradebook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolGradebook.Services
+{
+    public class SchoolService
+    {
+        private readonly SchoolContext context;
+
+        public SchoolService(SchoolContext context)
+            => this.context = context;
+
+        public async Task AddSchoolAsync(School school)
+        {
+            await context.Schools.AddAsync(school);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task UpdateSchoolAsync(School school)
+        {
+            context.Attach(school).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<School> GetSchoolById(int id)
+            => await context.Schools.Where(r => r.Id == id).AsNoTracking().FirstOrDefaultAsync();
+
+        public async Task<School[]> GetAllSchools()
+            => await context.Schools.AsNoTracking().ToArrayAsync();
+    }
+}

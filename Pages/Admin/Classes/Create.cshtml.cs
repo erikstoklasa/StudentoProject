3f4ccf65c911ca690b35a983078d0fb850a9fca8
Erik Stoklasa <45731908+erikstoklasa@users.noreply.github.com>
2020-09-22 20:39:20 +0200

    Bugfixed creating classes

--- Pages/Admin/Classes/Create.cshtml.cs
+++ Pages/Admin/Classes/Create.cshtml.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using SchoolGradebook.Data;
 using SchoolGradebook.Models;
 using SchoolGradebook.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace SchoolGradebook.Pages.Admin.Classes
 {
@@ -15,25 +15,38 @@ namespace SchoolGradebook.Pages.Admin.Classes
     {
         private readonly ClassService classService;
         private readonly TeacherService teacherService;
+        private readonly AdminService adminService;
+        private readonly RoomService roomService;
 
         public List<SelectListItem> TeachersList { get; set; }
+        public List<SelectListItem> RoomsList { get; set; }
         public List<Models.Teacher> Teachers { get; set; }
+        public string UserId { get; set; }
 
-        public CreateModel(ClassService classService, TeacherService teacherService)
+        public CreateModel(ClassService classService, TeacherService teacherService, IHttpContextAccessor httpContextAccessor, AdminService adminService, RoomService roomService)
         {
             this.classService = classService;
             this.teacherService = teacherService;
+            this.adminService = adminService;
+            this.roomService = roomService;
             TeachersList = new List<SelectListItem>();
+            RoomsList = new List<SelectListItem>();
+            UserId = httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            UserId ??= "";
         }
 
         public async Task<IActionResult> OnGet()
         {
             Teachers = (await teacherService.GetAllTeachersAsync()).ToList();
-            foreach(Models.Teacher t in Teachers)
+            foreach (Models.Teacher t in Teachers)
             {
                 TeachersList.Add(new SelectListItem(t.GetFullName(), t.Id.ToString()));
             }
-
+            var rooms = await roomService.GetAllRooms();
+            foreach (var room in rooms)
+            {
+                RoomsList.Add(new SelectListItem(room.Name, room.Id.ToString()));
+            }
             return Page();
         }
 
@@ -48,6 +61,7 @@ namespace SchoolGradebook.Pages.Admin.Classes
             {
                 return Page();
             }
+            Class.SchoolId = await adminService.GetAdminId(UserId);
             await classService.AddClassAsync(Class);
 
             return RedirectToPage("./Index");
--- Pages/Admin/Classes/Edit.cshtml.cs
+++ Pages/Admin/Classes/Edit.cshtml.cs
@@ -1,14 +1,11 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SchoolGradebook.Models;
+using SchoolGradebook.Services;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using SchoolGradebook.Data;
-using SchoolGradebook.Models;
-using SchoolGradebook.Services;
 
 namespace SchoolGradebook.Pages.Admin.Classes
 {
@@ -16,15 +13,19 @@ namespace SchoolGradebook.Pages.Admin.Classes
     {
         private readonly ClassService classService;
         private readonly TeacherService teacherService;
+        private readonly RoomService roomService;
 
         public List<SelectListItem> TeachersList { get; set; }
         public List<Models.Teacher> Teachers { get; set; }
+        public List<SelectListItem> RoomsList { get; set; }
 
-        public EditModel(ClassService classService, TeacherService teacherService)
+        public EditModel(ClassService classService, TeacherService teacherService, RoomService roomService)
         {
             this.classService = classService;
             this.teacherService = teacherService;
+            this.roomService = roomService;
             TeachersList = new List<SelectListItem>();
+            RoomsList = new List<SelectListItem>();
         }
 
         [BindProperty]
@@ -46,20 +47,25 @@ namespace SchoolGradebook.Pages.Admin.Classes
             Teachers = (await teacherService.GetAllTeachersAsync()).ToList();
             foreach (Models.Teacher t in Teachers)
             {
-                TeachersList.Add(new SelectListItem(t.GetFullName(), t.Id.ToString()));
+                bool teacherSelected = Class.TeacherId == t.Id;
+                TeachersList.Add(new SelectListItem(t.GetFullName(), t.Id.ToString(), teacherSelected));
+            }
+            var rooms = await roomService.GetAllRooms();
+            foreach (var room in rooms)
+            {
+                bool roomSelected = room.Id == Class.BaseRoomId;
+                RoomsList.Add(new SelectListItem(room.Name, room.Id.ToString(), roomSelected));
             }
             return Page();
         }
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
             {
                 return Page();
             }
-
+            Class.SchoolId = (await classService.GetClassAsync(Class.Id)).SchoolId;
             await classService.UpdateClassAsync(Class);
 
             return RedirectToPage("./Index");

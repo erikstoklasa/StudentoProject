f75a35b43bd466c3cc2339754dca1b866c5f6fc4
Fedor Stryapunin <fedor.stryapunin@gmail.com>
2020-11-26 23:35:49 +0100

    complete react setup

---
+++ Scripts/src/anotherone.js
@@ -0,0 +1,6 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Another = () => <div>Another Hello world!</div>;
+
+ReactDOM.render(<Another />, document.getElementById("another-root"));

6d8b1a49479db1a0f81b670313e3d50decb59200
Fedor Stryapunin <fedor.stryapunin@gmail.com>
2020-12-17 16:11:35 +0100

    final visual tweaks 2/2 #72

--- Scripts/src/Grades/Grade Page/SubComponents/ColumnHeader.js
+++ Scripts/src/Grades/Grade Page/SubComponents/ColumnHeader.js
@@ -1,24 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../GradePage.css';
 
 const ColumnHeader = ({ title, type, handleClick, displayInput, onInputChange, onClickHeader }) => {
+    const [displayArrow, updateDisplayArrow] = useState(false)
+
+    const handleAverageHeaderClick = () => {
+        updateDisplayArrow(!displayArrow)
+    }
+
 
     if (type === 'New Grade') {
         return (
             <div className="column-header">
             
-                {(!displayInput ? <div className=" column-header-container"><div className=" btn btn-primary btn-display" onClick={handleClick}><img src="/images/add.svg" alt="Přidat" height="18px" class="plus"></img><p className="column-header-text">{title}</p></div></div>
+                {(!displayInput ? <div className=" column-header-container"><div className=" btn btn-primary btn-display" onClick={handleClick}><img src="/images/add.svg" alt="Přidat" height="18px" className="plus"></img><p className="column-header-text">{title}</p></div></div>
                     : <div className="column-header-container"><input className="form-control column-header-input" onChange={onInputChange} /></div>)}
             
             </div>
         )
     }
 
-    if (type === 'average') { 
+
+
+
+    if (type === 'average' && !displayArrow) { 
         return (
-            <div className="column-header" onClick={onClickHeader}><p className="column-header-text">{title}</p></div>
+            <div className="column-header" onClick={() => {
+                onClickHeader()
+                handleAverageHeaderClick()
+            }}><img src="/images/uparrow.svg" height="18px" className="plus"></img><p className="column-header-text">{title}</p></div>
         ) 
     }
+
+    if (type === 'average' && displayArrow) { 
+        return (
+            <div className="column-header" onClick={() => {
+                onClickHeader()
+                handleAverageHeaderClick()
+            }}><img src="/images/downarrow.svg" height="18px" className="plus"></img><p className="column-header-text">{title}</p></div>
+        ) 
+    }
+
 
     else {
         return (

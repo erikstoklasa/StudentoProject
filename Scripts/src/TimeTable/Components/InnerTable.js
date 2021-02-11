b73c0514f74bb18378af5253f5d70c1c8169a699
Fedor Stryapunin <fedor.stryapunin@gmail.com>
2021-02-11 15:52:37 +0100

    timetamble final #110 & #111

--- Scripts/src/TimeTable/Components/InnerTable.js
+++ Scripts/src/TimeTable/Components/InnerTable.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import TableRow from './TableRow'
+import TimeRow from './TimeRow'
 
-const InnerTable = ({ sortedData, week }) => {
+const InnerTable = ({ sortedData, week, type }) => {
     if (sortedData && week) {
         const actualWeek = sortedData.filter(weekObject => {
             return weekObject.week === week
         })
         console.log(actualWeek)
-    
         return (
             <div className="inner-table">
-                <TableRow frameArray={actualWeek[0].timeFrames[0]} />
-                <TableRow frameArray={actualWeek[0].timeFrames[1]} />
-                <TableRow frameArray={actualWeek[0].timeFrames[2]} />
-                <TableRow frameArray={actualWeek[0].timeFrames[3]} />
-                <TableRow frameArray={actualWeek[0].timeFrames[4]}/>
+                <TimeRow frameArray={actualWeek[0].timeFrames[0]}/>
+                <TableRow frameArray={actualWeek[0].timeFrames[0]} dayName={'Pondělí'} type={type}/>
+                <TableRow frameArray={actualWeek[0].timeFrames[1]} dayName={'Úterý'} type={type}/>
+                <TableRow frameArray={actualWeek[0].timeFrames[2]} dayName={'Středa'} type={type}/>
+                <TableRow frameArray={actualWeek[0].timeFrames[3]} dayName={'Čtvrtek'} type={type}/>
+                <TableRow frameArray={actualWeek[0].timeFrames[4]} dayName={'Pátek'} type={type}/>
             </div>
         )
     }
--- Scripts/src/TimeTable/Components/TableCell.js
+++ Scripts/src/TimeTable/Components/TableCell.js
@@ -1,20 +1,55 @@
 import React from 'react'
 import '../TimeTable.css'
 
-const TableCell = ({ frame }) => {
+const TableCell = ({ frame, type }) => {
    
-    if (frame.timetableEntry) {
-        const heading = frame.timetableEntry.subjectInstanceName.substring(0,3)
-        return (
-            <div className="table-cell">
-                <div className="subject-heading">{heading}</div>
-            </div>
-        )
-    }
+    if (frame.timetableEntry && !frame.timetableEntryChange) {
+        const heading = frame.timetableEntry.subjectInstanceName       
+        if (type === "Teacher") {
+            const group = frame.timetableEntry.group
+            return (
+                <div className="table-cell">
+                    <div className="room-number">{frame.timetableEntry.room}</div>
+                    <div className="subject-heading">{heading}</div>
+                    <div className="teacher-name">{group}</div>   
+                </div>
+            )
+        }
+        else {
+            const teacher = frame.timetableEntry.teacherFirstName.substring(0, 1).concat(`. ${frame.timetableEntry.teacherLastName}`)
+            return (
+                <div className="table-cell">
+                    <div className="room-number">{frame.timetableEntry.room}</div>
+                    <div className="subject-heading">{heading}</div>
+                    <div className="teacher-name">{`${teacher}`}</div>
+                </div>
+            )
+        }
+    } else if (frame.timetableEntryChange) {
+        const heading = frame.timetableEntryChange.subjectInstanceName
+        if (type === "Teacher") {
+            const group = frame.timetableEntry.group
+            return (
+                <div className="table-cell">
+                    <div className="room-number">{frame.timetableEntry.room}</div>
+                    <div className="subject-heading">{heading}</div>
+                    <div className="teacher-name">{group}</div>
+                </div>
+            )
+        } else {        
+            const teacher = frame.timetableEntryChange.teacherFirstName.substring(0, 1).concat(`. ${frame.timetableEntry.teacherLastName}`)
+            return (
+                <div className="table-cell-change">
+                    <div className="room-number">{frame.timetableEntryChange.room}</div>
+                    <div className="subject-heading">{heading}</div>
+                    <div className="teacher-name">{`${teacher}`}</div>
+                </div>
+            )
+        }
+     }
     else { 
         return (
-            <div className="table-cell">
-                <div className="subject-heading"></div>
+            <div className="table-cell-empty">                
             </div>
         )
     }

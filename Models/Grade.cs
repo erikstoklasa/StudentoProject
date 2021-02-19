a136d91102bc667091b6b77d7f46670593b523cc
Erik Stoklasa <45731908+erikstoklasa@users.noreply.github.com>
2021-02-19 11:50:46 +0100

    Updated Grade model

--- Models/Grade.cs
+++ Models/Grade.cs
@@ -8,19 +8,25 @@ namespace SchoolGradebook.Models
 {
     public class Grade
     {
+        public enum USERTYPE { Teacher, Student }
+
         public int Id { get; set; }
         [Display(Name = "Název")]
         [Required(ErrorMessage = "Zadejte prosím název")]
         public string Name { get; set; }
         [Display(Name = "Známka")]
-        [Range(1,5, ErrorMessage = "Zadejte známku mezi 1 a 5")]
+        [Range(1, 5, ErrorMessage = "Zadejte známku mezi 1 a 5")]
         public int? Value { get; set; }
-        public int StudentId { get; set; } 
+        public int StudentId { get; set; }
         public int SubjectInstanceId { get; set; }
+        public USERTYPE AddedBy { get; set; }
+        //Reference new model GradeGroup (name, scaleType)
+        //public int GradeGroupId { get; set; }
         [Display(Name = "Datum přidání")]
         public DateTime Added { get; set; }
 
         public Student Student { get; set; }
         public SubjectInstance SubjectInstance { get; set; }
     }
+
 }

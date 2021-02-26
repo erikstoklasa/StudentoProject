2402595120af90e0cdc6c89db3d5ca6f5fbb6817
Fedor Stryapunin <fedor.stryapunin@gmail.com>
2021-02-26 11:03:46 +0100

    fixed nonsensical dates sometimes displaying at the end of the month (eg 33.2)

--- Scripts/src/TimeTable/Components/InnerTable.js
+++ Scripts/src/TimeTable/Components/InnerTable.js
@@ -12,27 +12,33 @@ const InnerTable = ({ sortedData, week, type, displayVertical }) => {
         const weekStart = actualWeek[0].weekStart.split('T')[0]; 
         const dateArray = weekStart.split('-').map(number => {            
             return parseInt(number)
-        });        
+        });
+        
+        //add number of days in a given month to date array
+        dateArray.push(new Date(dateArray[1], dateArray[0], 0).getDate())
+       
+
+
         if (!displayVertical) {
             return (
                 <div className="inner-table">
                     <TimeRow frameArray={actualWeek[0].timeFrames[0]} />
-                    <TableRow frameArray={actualWeek[0].timeFrames[0]} dayName={'Pondělí'} type={type} date={`${dateArray[2]}.${dateArray[1]}`} />
-                    <TableRow frameArray={actualWeek[0].timeFrames[1]} dayName={'Úterý'} type={type} date={`${dateArray[2] + 1}.${dateArray[1]}`} />
-                    <TableRow frameArray={actualWeek[0].timeFrames[2]} dayName={'Středa'} type={type} date={`${dateArray[2] + 2}.${dateArray[1]}`} />
-                    <TableRow frameArray={actualWeek[0].timeFrames[3]} dayName={'Čtvrtek'} type={type} date={`${dateArray[2] + 3}.${dateArray[1]}`} />
-                    <TableRow frameArray={actualWeek[0].timeFrames[4]} dayName={'Pátek'} type={type} date={`${dateArray[2] + 4}.${dateArray[1]}`} />
+                    <TableRow frameArray={actualWeek[0].timeFrames[0]} dayName={'Pondělí'} type={type}  dateModifier={0} dateArray={dateArray} />
+                    <TableRow frameArray={actualWeek[0].timeFrames[1]} dayName={'Úterý'} type={type}  dateModifier={1} dateArray={dateArray}/>
+                    <TableRow frameArray={actualWeek[0].timeFrames[2]} dayName={'Středa'} type={type}  dateModifier={2} dateArray={dateArray}/>
+                    <TableRow frameArray={actualWeek[0].timeFrames[3]} dayName={'Čtvrtek'} type={type}  dateModifier={3} dateArray={dateArray}/>
+                    <TableRow frameArray={actualWeek[0].timeFrames[4]} dayName={'Pátek'} type={type} dateModifier={4} dateArray={dateArray}/>
                 </div>
             )
         } else { 
             return (
                 <div className="inner-table inner-table-vertical">
                     <TimeColumn frameArray={actualWeek[0].timeFrames[0]} />
-                    <TableColumn frameArray={actualWeek[0].timeFrames[0]} dayName={'Pondělí'} type={type} date={`${dateArray[2]}.${dateArray[1]}`} />
-                    <TableColumn frameArray={actualWeek[0].timeFrames[1]} dayName={'Úterý'} type={type} date={`${dateArray[2] + 1}.${dateArray[1]}`} />
-                    <TableColumn frameArray={actualWeek[0].timeFrames[2]} dayName={'Středa'} type={type} date={`${dateArray[2] + 2}.${dateArray[1]}`} />
-                    <TableColumn frameArray={actualWeek[0].timeFrames[3]} dayName={'Čtvrtek'} type={type} date={`${dateArray[2] + 3}.${dateArray[1]}`} />
-                    <TableColumn frameArray={actualWeek[0].timeFrames[4]} dayName={'Pátek'} type={type} date={`${dateArray[2] + 4}.${dateArray[1]}`} />
+                    <TableColumn frameArray={actualWeek[0].timeFrames[0]} dayName={'Pondělí'} type={type} dateModifier={0} dateArray={dateArray} />
+                    <TableColumn frameArray={actualWeek[0].timeFrames[1]} dayName={'Úterý'} type={type} dateModifier={1} dateArray={dateArray} />
+                    <TableColumn frameArray={actualWeek[0].timeFrames[2]} dayName={'Středa'} type={type} dateModifier={2} dateArray={dateArray} />
+                    <TableColumn frameArray={actualWeek[0].timeFrames[3]} dayName={'Čtvrtek'} type={type} dateModifier={3} dateArray={dateArray} />
+                    <TableColumn frameArray={actualWeek[0].timeFrames[4]} dayName={'Pátek'} type={type} dateModifier={4} dateArray={dateArray} />
                 </div>
             )
         }
--- Scripts/src/TimeTable/Components/TableRow.js
+++ Scripts/src/TimeTable/Components/TableRow.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import TableCell from './TableCell'
 import '../TimeTable.css'
 
-const TableRow = ({ frameArray, dayName, type, date }) => {  
-   
+const TableRow = ({ frameArray, dayName, type, dateArray, dateModifier }) => {  
+    const [actualDate, updateActualDate] = useState();
     const cellArray = frameArray.map(frame => {
         return <TableCell frame={frame} type={type}/>
-    })
+    })   
+
+    const determineDate = () => { 
+        if (dateArray[2] + dateModifier <= dateArray[3]) {
+            updateActualDate(`${dateArray[2] + dateModifier}.${dateArray[1]}`)
+        } else {           
+            const spillOverToTheNextMonth = dateArray[3] - (dateArray[2] + dateModifier)
+            const nextMonthDay = 0 - spillOverToTheNextMonth;
+
+            updateActualDate(`${nextMonthDay}.${dateArray[1] + 1}`)
+        }
+    }
+
+    useEffect(determineDate, dateArray)
 
     return (
         <div className="table-row">
         <div className="day-cell">
                 <h5 >{dayName}</h5>
-                <p className="row-date">{date}</p>
+                <p className="row-date">{actualDate}</p>
         </div>
             {cellArray}
         </div>

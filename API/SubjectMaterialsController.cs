be3241857a635b382ab741542f4873d5761f5cc6
Erik Stoklasa <45731908+erikstoklasa@users.noreply.github.com>
2021-03-31 14:54:07 +0200

    Added soft delete endpoint to the study materials api

--- API/SubjectMaterialsController.cs
+++ API/SubjectMaterialsController.cs
@@ -392,6 +392,25 @@ namespace SchoolGradebook.API.SubjectMaterials
             return BadRequest();
         }
 
+        /// <summary>
+        /// Soft deletes subject material group
+        /// </summary>
+        /// <param name="subjectMaterialId"></param>
+        /// <returns></returns>
+        [HttpDelete("Student/Material")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [Authorize(policy: "OnlyStudent")]
+        public async Task<ActionResult> StudentDeleteSubjectMaterial(Guid subjectMaterialId)
+        {
+            int studentId = await studentService.GetStudentId(UserId);
+            if (!await studentAccessValidation.HasAccessToSubjectMaterial(studentId, subjectMaterialId))
+            {
+                return StatusCode(403);
+            }
+            await subjectMaterialService.SoftDeleteMaterialAsync(subjectMaterialId);
+            return Ok();
+        }
 
         /// <summary>
         /// Adds subject material group for student

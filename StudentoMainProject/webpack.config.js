92927616fe8e54c374bf27e7d0c274c2f5baa859
saltyfedor <fedor.stryapunin@gmail.com>
2021-09-06 19:53:19 +0200

    webpack config fix #184

--- StudentoMainProject/webpack.config.js
+++ StudentoMainProject/webpack.config.js
@@ -4,8 +4,8 @@ const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 module.exports = {
     entry: { 
         
-        /*TimeTable: "./Scripts/src/TimeTable/index.js",  */      
-       // Grades: "./Scripts/src/Grades/index.js",
+        TimeTable: "./Scripts/src/TimeTable/index.js",       
+        Grades: "./Scripts/src/Grades/index.js",
         SubjectDetail: "./Scripts/src/SubjectDetail/index.js",
         SubjectDetailTeacher: "./Scripts/src/SubjectDetailTeacher/index.js"
 

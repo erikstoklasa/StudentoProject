f3e866a68fc2e14e3f03a6f41c2d8533e95adc0f
Erik Stoklasa <45731908+erikstoklasa@users.noreply.github.com>
2021-09-20 22:31:31 +0200

    Updated database seeder, fixed grades - now they are seeded based on current subject instances

--- StudentoMainProject/Data/DbInitializer.cs
+++ StudentoMainProject/Data/DbInitializer.cs
@@ -4,12 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Globalization;
+using Microsoft.EntityFrameworkCore;
 
 namespace SchoolGradebook.Data
 {
     public class DbInitializer
     {
-        public static void Initialize(SchoolContext context)
+        public static async Task Initialize(SchoolContext context)
         {
             System.Threading.Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("cs-CZ");
             //if none is filled in then return
@@ -20,11 +21,12 @@ namespace SchoolGradebook.Data
             string[] lastNames = { "Nováková", "Novák", "Svobodová", "Svoboda", "Novotný", "Novotná", "Dvořáková", "Dvořák", "Černá", "Černý", "Procházková", "Procházka", "Kučerová", "Kučera", "Veselá", "Veselý" };
             DateTime[] dates = { DateTime.Parse("07/09/2019"), DateTime.Parse("10/12/2019"), DateTime.Parse("11/01/2020"), DateTime.Parse("18/02/2020"), DateTime.Parse("05/03/2020"), DateTime.Parse("28/03/2020"), DateTime.Parse("11/04/2020") };
             DateTime[] birthdays = { DateTime.Parse("07/09/2002"), DateTime.Parse("10/12/2002"), DateTime.Parse("21/01/2002"), DateTime.Parse("18/02/2002"), DateTime.Parse("05/03/2002"), DateTime.Parse("28/03/2002"), DateTime.Parse("11/04/2002") };
-            const int NUMBER_OF_TEACHERS = 10;
+            const int NUMBER_OF_TEACHERS = 6;
             const int NUMBER_OF_STUDENTS = 15;
             const int NUMBER_OF_CLASSES = 20;
             const int NUMBER_OF_SUBJECTTYPES = 6;
-            const int NUMBER_OF_SUBJECTINSTANCES = NUMBER_OF_TEACHERS * NUMBER_OF_SUBJECTTYPES;
+            const int NUMBER_OF_SUBJECTS_PER_TEACHER = 3;
+            const int NUMBER_OF_SUBJECTINSTANCES = NUMBER_OF_SUBJECTS_PER_TEACHER * NUMBER_OF_TEACHERS;
             string[] classesNames = { "A", "B", "C", "D" };
             int classesPerYear = classesNames.Length;
 
@@ -38,8 +40,8 @@ namespace SchoolGradebook.Data
                     new School() { Name = "Gymnázium Jana Keplera", CityAddress = "Praha 6", StreetAddress = "Parléřova 117", Email = "gjk@jk.cz", OrganizationIdentifNumber = "123456789", PhoneNumber = "123456789", Website = "https://gjk.cz", ZipCode = "16000" }
                 };
 
-                context.Schools.AddRange(schools);
-                context.SaveChanges();
+                await context.Schools.AddRangeAsync(schools);
+                await context.SaveChangesAsync();
             }
 
             //Teachers
@@ -52,8 +54,8 @@ namespace SchoolGradebook.Data
                     teachers.Add(new Teacher { FirstName = firstNames[r.Next(firstNames.Length)], LastName = lastNames[r.Next(lastNames.Length)], SchoolId = 1, Birthdate = birthdays[r.Next(birthdays.Length)], PersonalIdentifNumber = "11" });
                 }
 
-                context.Teachers.AddRange(teachers);
-                context.SaveChanges();
+                await context.Teachers.AddRangeAsync(teachers);
+                await context.SaveChangesAsync();
             }
             //Subject types
             if (!context.SubjectTypes.Any())
@@ -68,26 +70,25 @@ namespace SchoolGradebook.Data
                         new SubjectType{Name = "Programování", SpecializationName = "Gymnázium", SchoolId=1}
                     };
 
-                context.SubjectTypes.AddRange(subjectTypes);
-                context.SaveChanges();
+                await context.SubjectTypes.AddRangeAsync(subjectTypes);
+                await context.SaveChangesAsync();
             }
 
             //Subject instances
             if (!context.SubjectInstances.Any())
             {
                 List<SubjectInstance> subjectInstances = new();
-                for (int i = 1; i <= NUMBER_OF_SUBJECTTYPES; i++)
+                for (int i = 1; i <= NUMBER_OF_TEACHERS; i++)
                 {
-                    for (int y = 1; y <= NUMBER_OF_TEACHERS; y++)
+                    for (int y = 0; y < NUMBER_OF_SUBJECTS_PER_TEACHER; y++)
                     {
-                        subjectInstances.Add(new SubjectInstance { TeacherId = y, SubjectTypeId = i });
+                        subjectInstances.Add(new SubjectInstance { TeacherId = i, SubjectTypeId = r.Next(1, NUMBER_OF_SUBJECTTYPES) });
                     }
                 }
 
-                context.SubjectInstances.AddRange(subjectInstances);
-                context.SaveChanges();
+                await context.SubjectInstances.AddRangeAsync(subjectInstances);
+                await context.SaveChangesAsync();
             }
-
 
 
             // Rooms
@@ -95,32 +96,32 @@ namespace SchoolGradebook.Data
             {
                 var rooms = new Room[]
                 {
-                    new Room{Name="101", SchoolId=1},
-                    new Room{Name="102", SchoolId=1},
-                    new Room{Name="103", SchoolId=1},
-                    new Room{Name="104", SchoolId=1},
-                    new Room{Name="105", SchoolId=1}
+                    new Room{Name="H1.1", SchoolId=1},
+                    new Room{Name="H1.2", SchoolId=1},
+                    new Room{Name="H1.3", SchoolId=1},
+                    new Room{Name="H2.1", SchoolId=1},
+                    new Room{Name="H2.2", SchoolId=1}
                 };
 
 
-                context.Rooms.AddRange(rooms);
-                context.SaveChanges();
+                await context.Rooms.AddRangeAsync(rooms);
+                await context.SaveChangesAsync();
             }
             //Classes
             if (!context.Classes.Any())
             {
                 List<Class> classes = new();
-                short years = (short)(NUMBER_OF_CLASSES / classesPerYear);
-                for (int i = 1; i <= years; i++)
+                short yearCount = (short)(NUMBER_OF_CLASSES / classesPerYear);
+                for (int year = 1; year <= yearCount; year++)
                 {
-                    for (int y = 0; y < classesPerYear; y++)
+                    for (int classInYear = 0; classInYear < classesPerYear; classInYear++)
                     {
-                        classes.Add(new Class() { Grade = (short)i, Name = classesNames[y], BaseRoomId = 1, TeacherId = 1, SchoolId = 1 });
+                        classes.Add(new Class() { Grade = (short)year, Name = classesNames[classInYear], BaseRoomId = r.Next(1, 6), TeacherId = 1, SchoolId = 1 });
                     }
 
                 }
-                context.Classes.AddRange(classes);
-                context.SaveChanges();
+                await context.Classes.AddRangeAsync(classes);
+                await context.SaveChangesAsync();
             }
             //Students
             if (!context.Students.Any())
@@ -129,60 +130,47 @@ namespace SchoolGradebook.Data
 
                 for (int i = 0; i < NUMBER_OF_STUDENTS; i++)
                 {
-                    students.Add(new Student { FirstName = firstNames[r.Next(firstNames.Length)], LastName = lastNames[r.Next(lastNames.Length)], ClassId = 1, SchoolId = 1, Birthdate = birthdays[r.Next(birthdays.Length)], PersonalIdentifNumber = "11" });
+                    students.Add(
+                        new Student
+                        {
+                            FirstName = firstNames[r.Next(firstNames.Length)],
+                            LastName = lastNames[r.Next(lastNames.Length)],
+                            ClassId = 1,
+                            SchoolId = 1,
+                            Birthdate = birthdays[r.Next(birthdays.Length)],
+                            PersonalIdentifNumber = "11"
+                        });
                 }
-                context.Students.AddRange(students);
-                context.SaveChanges();
+                await context.Students.AddRangeAsync(students);
+                await context.SaveChangesAsync();
             }
             //GradeGroups
             if (!context.GradeGroups.Any())
             {
+
                 List<GradeGroup> gradeGroups = new();
                 string[] gradeGroupNames = { "Domácí úkol", "Test", "Čtvrtletní práce", "Prezentace", "Aktivita při hodině" };
-                for (int i = 0; i < gradeGroupNames.Length; i++)
+
+                List<SubjectInstance> subjectInstances = await context.SubjectInstances
+                    .AsNoTracking()
+                    .ToListAsync();
+                foreach (var si in subjectInstances)
                 {
-                    var g = new GradeGroup
+                    foreach (var gradeGroupName in gradeGroupNames)
                     {
-                        Name = gradeGroupNames[i],
-                        Weight = 5,
-                    };
-                    gradeGroups.Add(g);
-                }
-                context.GradeGroups.AddRange(gradeGroups);
-                context.SaveChanges();
-            }
-            //Grades
-            if (!context.Grades.Any())
-            {
-                int[] allowedValues = { -10, 0, 10, 15, 25, 35, 40, 50, 60, 65, 75, 85, 90, 100, 110 };
-                List<Grade> grades = new();
-                for (int i = 1; i <= NUMBER_OF_STUDENTS; i++)
-                {
-                    for (int y = 1; y <= NUMBER_OF_SUBJECTINSTANCES; y++)
-                    {
-                        for (int z = 1; z <= 5; z++)
+                        var g = new GradeGroup
                         {
-                            var g = new Grade
-                            {
-                                Name = "",
-                                StudentId = i,
-                                SubjectInstanceId = y,
-                                GradeGroupId = z,
-                                Added = dates[r.Next(dates.Length)]
-                            };
-                            g.SetGradeValue(
-                                allowedValues[
-                                    r.Next(0, allowedValues.Length)
-                                    ]
-                                );
-                            grades.Add(g);
-                        }
-
+                            Name = gradeGroupName,
+                            Weight = r.Next(1, 11),
+                            AddedBy = GradeGroup.USERTYPE.Teacher,
+                            AddedById = si.TeacherId
+                        };
+                        gradeGroups.Add(g);
                     }
-
                 }
-                context.Grades.AddRange(grades);
-                context.SaveChanges();
+
+                await context.GradeGroups.AddRangeAsync(gradeGroups);
+                await context.SaveChangesAsync();
             }
             //TimeFrames
             if (!context.TimeFrames.Any())
@@ -202,8 +190,8 @@ namespace SchoolGradebook.Data
                     timeFrames.Add(new TimeFrame() { DayOfWeek = (DayOfWeek)i, Start = DateTime.Parse("17:00:00"), End = DateTime.Parse("17:45:00"), SchoolId = 1 });
                     timeFrames.Add(new TimeFrame() { DayOfWeek = (DayOfWeek)i, Start = DateTime.Parse("17:50:00"), End = DateTime.Parse("18:35:00"), SchoolId = 1 });
                 }
-                context.TimeFrames.AddRange(timeFrames);
-                context.SaveChanges();
+                await context.TimeFrames.AddRangeAsync(timeFrames);
+                await context.SaveChangesAsync();
             }
             //TimetableRecords
             if (!context.TimetableRecords.Any())
@@ -213,8 +201,8 @@ namespace SchoolGradebook.Data
                 {
                     timetableRecords.Add(new TimetableRecord() { SubjectInstanceId = r.Next(1, 8), RoomId = r.Next(1, 4), Recurrence = 1, TimeFrameId = r.Next(1, 56) });
                 }
-                context.TimetableRecords.AddRange(timetableRecords);
-                context.SaveChanges();
+                await context.TimetableRecords.AddRangeAsync(timetableRecords);
+                await context.SaveChangesAsync();
             }
             //StudentGroups
             if (!context.StudentGroups.Any())
@@ -225,8 +213,8 @@ namespace SchoolGradebook.Data
                     new StudentGroup{Name="Skupina2", SchoolId=1}
                 };
 
-                context.StudentGroups.AddRange(groups);
-                context.SaveChanges();
+                await context.StudentGroups.AddRangeAsync(groups);
+                await context.SaveChangesAsync();
             }
             //TimetableChanges
             if (!context.TimetableChanges.Any())
@@ -236,19 +224,29 @@ namespace SchoolGradebook.Data
                 {
                     timetableChanges.Add(new TimetableChange() { SubjectInstanceId = r.Next(1, 8), StudentGroupId = r.Next(1, 3), Week = r.Next(1, 50), TimeFrameId = r.Next(1, 36), Canceled = r.Next(1, 3) == 1 });
                 }
-                context.TimetableChanges.AddRange(timetableChanges);
-                context.SaveChanges();
+                await context.TimetableChanges.AddRangeAsync(timetableChanges);
+                await context.SaveChangesAsync();
             }
             //StudentGroupEnrollments
             if (!context.StudentGroupEnrollments.Any())
             {
-                var entrollments = new StudentGroupEnrollment[]
+                List<Student> students = await context.Students.AsNoTracking().ToListAsync();
+                List<StudentGroupEnrollment> enrollments = new();
+                foreach (var student in students)
                 {
-                    new StudentGroupEnrollment{StudentId=1, StudentGroupId=1}
-                };
+                    int studentGroupId = student.Id % 2 == 0 ? 1 : 2; //Distributing students into two student groups - one to the first student group and the other to the second student group
 
-                context.StudentGroupEnrollments.AddRange(entrollments);
-                context.SaveChanges();
+                    enrollments.Add(
+                        new StudentGroupEnrollment
+                        {
+                            StudentId = student.Id,
+                            StudentGroupId = studentGroupId
+                        });
+
+                }
+
+                await context.StudentGroupEnrollments.AddRangeAsync(enrollments);
+                await context.SaveChangesAsync();
             }
 
             //Enrollments
@@ -267,8 +265,45 @@ namespace SchoolGradebook.Data
                         subjectInstanceEnrollments.Add(new SubjectInstanceEnrollment { StudentGroupId = 2, SubjectInstanceId = i });
                     }
                 }
-                context.Enrollments.AddRange(subjectInstanceEnrollments);
-                context.SaveChanges();
+                await context.Enrollments.AddRangeAsync(subjectInstanceEnrollments);
+                await context.SaveChangesAsync();
+            }
+            //Grades
+            if (!context.Grades.Any())
+            {
+                int[] allowedValues = { -10, 0, 10, 15, 25, 35, 40, 50, 60, 65, 75, 85, 90, 100, 110 };
+                List<Grade> grades = new();
+                List<SubjectInstance> subjectInstances =
+                    await context.SubjectInstances.AsNoTracking().ToListAsync();
+                List<Student> students =
+                    await context.Students.AsNoTracking().ToListAsync();
+                foreach (var si in subjectInstances)
+                {
+                    foreach (var student in students)
+                    {
+                        for (int gradeGroupId = 1; gradeGroupId <= 5; gradeGroupId++)
+                        {
+                            var g = new Grade
+                            {
+                                Name = "",
+                                StudentId = student.Id,
+                                SubjectInstanceId = si.Id,
+                                GradeGroupId = gradeGroupId,
+                                Added = dates[r.Next(dates.Length)]
+                            };
+                            g.SetGradeValue(
+                                allowedValues[
+                                    r.Next(allowedValues.Length)
+                                    ]
+                                );
+                            grades.Add(g);
+                        }
+
+                    }
+
+                }
+                await context.Grades.AddRangeAsync(grades);
+                await context.SaveChangesAsync();
             }
             //HumanActivationCodes
             if (!context.HumanActivationCodes.Any())
@@ -279,8 +314,8 @@ namespace SchoolGradebook.Data
                 new HumanActivationCode{TargetId=1, HumanCode="111111", CodeType = CodeType.Student}
 
                 };
-                context.HumanActivationCodes.AddRange(humanActivationCodes);
-                context.SaveChanges();
+                await context.HumanActivationCodes.AddRangeAsync(humanActivationCodes);
+                await context.SaveChangesAsync();
             }
 
         }

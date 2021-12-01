9842735206804b5d186ec06b81f6167b750830a0
Erik Stoklasa <me@erikstoklasa.cz>
2021-12-01 20:37:42 +0100

    Added simple import of students for admin

--- StudentoMainProject/Pages/Admin/Import/Index.cshtml.cs
+++ StudentoMainProject/Pages/Admin/Import/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Azure;
 using Azure.Storage.Blobs;
@@ -11,6 +13,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using SchoolGradebook.Services;
 using SendGrid;
 using SendGrid.Helpers.Mail;
 
@@ -19,12 +22,18 @@ namespace SchoolGradebook.Pages.Admin.Import
     public class IndexModel : PageModel
     {
         private readonly IConfiguration _configuration;
+        private readonly StudentService studentService;
+        private readonly AdminService adminService;
 
         public IFormFile FileUpload { get; set; }
+        private string UserId { get; set; }
 
-        public IndexModel(IConfiguration configuration)
+        public IndexModel(IHttpContextAccessor httpContextAccessor, IConfiguration configuration, StudentService studentService, AdminService adminService)
         {
             _configuration = configuration;
+            this.studentService = studentService;
+            this.adminService = adminService;
+            UserId = httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
         }
         public void OnGet()
         {
@@ -38,25 +47,53 @@ namespace SchoolGradebook.Pages.Admin.Import
                 using var stream = System.IO.File.Create(filePath);
                 await FileUpload.CopyToAsync(stream);
                 stream.Close();
-                Response<BlobContentInfo> response = null;
-                try
-                {
-                    response = await UploadToAzureBlobStorage(filePath);
-                    //TODO:log filename into db
-                }
-                catch (RequestFailedException e){
-                    ViewData["status"] = e.ToString();
-                }
-                if(response != null)
-                {
-                    ViewData["status"] = "OK";
-                }
-            }
-            await SendEmail(
-                "me@erikstoklasa.cz",
-                "Importování začalo",
-                "<h1>Status importování studentů</h1><p>Přijali jsme Vaše záznamy o studentech, jakmile budou naimportovány, tak Vás informujeme emailem. Může to trvat několik hodin.</p>");
+                using var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                using var sr = new StreamReader(fs, Encoding.UTF8);
 
+                string line = String.Empty;
+                List<Models.Student> students = new();
+                int schoolId = (await adminService.GetAdminByUserAuthId(UserId)).SchoolId;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] entries = line.Split(',');
+                    if (entries.Length != 6)
+                    {
+                        ViewData["status"] = "Nesprávný formát souboru";
+                        Response<BlobContentInfo> response = null;
+                        try
+                        {
+                            response = await UploadToAzureBlobStorage(filePath);
+                        }
+                        catch (RequestFailedException)
+                        {
+                        }
+                        return Page();
+                    }
+                    students.Add(new Models.Student()
+                    {
+                        FirstName = entries[0],
+                        LastName = entries[1],
+                        Birthdate = DateTime.Parse(entries[2]),
+                        PersonalIdentifNumber = entries[3],
+                        Email = entries[4],
+                        ClassId = string.IsNullOrWhiteSpace(entries[5]) ? null : int.Parse(entries[5]),
+                        SchoolId = schoolId
+                    });
+                }
+                var success = await studentService.AddStudentRangeAsync(students);
+                if (!success)
+                {
+                    ViewData["status"] = "Studenti nemají všechny požadované hodnoty, nebo je nešlo vložit do databáze";
+                    return Page();
+                }
+
+            }
+            //await SendEmail(
+            //    "me@erikstoklasa.cz",
+            //    "Importování začalo",
+            //    "<h1>Status importování studentů</h1><p>Přijali jsme Vaše záznamy o studentech, jakmile budou naimportovány, tak Vás informujeme emailem. Může to trvat několik hodin.</p>");
+            ViewData["status"] = "OK";
             return Page();
         }
         public async Task<Response<BlobContentInfo>> UploadToAzureBlobStorage(string filePath)

81524d1aca535ab270b67eb3b38b02878b29c0c3
Erik Stoklasa <me@erikstoklasa.cz>
2022-01-08 00:15:14 +0100

    Added logging service to auth api, pages and materials downloads, refactorings

--- StudentoMainProject/Pages/Teacher/Subjects/Materials/Details.cshtml.cs
+++ StudentoMainProject/Pages/Teacher/Subjects/Materials/Details.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Azure;
 using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -7,8 +8,12 @@ using Microsoft.Extensions.Configuration;
 using MimeTypes;
 using SchoolGradebook.Models;
 using SchoolGradebook.Services;
+using StudentoMainProject.Models;
+using StudentoMainProject.Services;
 using System;
 using System.IO;
+using System.Net;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace SchoolGradebook.Pages.Teacher.Subjects.Materials
@@ -17,20 +22,39 @@ namespace SchoolGradebook.Pages.Teacher.Subjects.Materials
     {
         private readonly IConfiguration _configuration;
         private readonly SubjectMaterialService subjectMaterialService;
+        private readonly LogItemService logItemService;
+        private readonly TeacherService teacherService;
 
-        public DetailsModel(IConfiguration configuration, SubjectMaterialService subjectMaterialService)
+        public DetailsModel(IConfiguration configuration,
+            SubjectMaterialService subjectMaterialService,
+            LogItemService logItemService,
+            IHttpContextAccessor httpContextAccessor,
+            TeacherService teacherService)
         {
             _configuration = configuration;
             this.subjectMaterialService = subjectMaterialService;
+            this.logItemService = logItemService;
+            this.teacherService = teacherService;
+            UserId = httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            IPAddress = httpContextAccessor.HttpContext.Connection.RemoteIpAddress;
         }
 
         public SubjectMaterial SubjectMaterial { get; set; }
+        public string UserId { get; set; }
+        public IPAddress IPAddress { get; set; }
 
         public async Task<IActionResult> OnGetAsync(Guid? id)
         {
             if (id == null)
             {
                 return NotFound();
+            }
+
+            int teacherId = await teacherService.GetTeacherId(UserId);
+
+            if (teacherId == -1)
+            {
+                return Forbid();
             }
 
             SubjectMaterial = await subjectMaterialService.GetMaterialAsync((Guid)id);
@@ -41,13 +65,23 @@ namespace SchoolGradebook.Pages.Teacher.Subjects.Materials
             }
 
             string connectionString = _configuration.GetConnectionString("AZURE_STORAGE_CONNECTION_STRING");
-            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            BlobServiceClient blobServiceClient = new(connectionString);
             BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("subjectmaterials");
             BlobClient blobClient = containerClient.GetBlobClient($"{id}{SubjectMaterial.FileExt}");
             var downloadFileStream = new MemoryStream();
             try
             {
                 Response r = await blobClient.DownloadToAsync(downloadFileStream);
+                await logItemService.Log(
+                new LogItem
+                {
+                    EventType = "TeacherMaterial",
+                    Timestamp = DateTime.UtcNow,
+                    UserAuthId = UserId,
+                    UserId = teacherId,
+                    UserRole = "teacher",
+                    IPAddress = IPAddress.ToString(),
+                });
             }
             catch (Exception)
             {
